5061696c8fdc50a3519435a99df53cf97cd72ed4
yevmikh <140793034+yevmikh@users.noreply.github.com>
2024-05-21 06:49:12 +0000

    Deploying to gh-pages from @ yevmikh/camper-test@eb0652db5bb1a3be51bc648c138362a269317d4e 🚀

--- static/js/359.be410677.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[359],{359:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"Favorites Page"})})}}}]);
-//# sourceMappingURL=359.be410677.chunk.js.map
